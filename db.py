0447440317501acd2197f1c7813851634a482a60
Alessandro <51935753+ALesssnddtro@users.noreply.github.com>
2022-03-07 00:26:00 +0800

    Update db.py

--- db.py
+++ db.py
@@ -13,12 +13,13 @@ c.execute('''CREATE TABLE User
              Weight int,
              TargetWeight int, 
              Height int,
+             Gender text,
              Reminders int,
-             Gender text)''')
+             Bmi int)''')
 
 c.execute('''CREATE TABLE Sessions
              (SessionID int PRIMARY KEY,
-             UserID time,
+             UserID int,
              StartDate date,
              StartTime time,
              Duration int,
@@ -72,13 +73,55 @@ c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurnt
 VALUES (3, "Squats", "Leg-day", 5.5)''')
 
 c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
-VALUES (4, "Hammer Curl", "Pull-day", 2.2)''')
+VALUES (4, "Hammer Curl", "Pull-day", 3.4)''')
 
 c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
-VALUES (5, "DB Shrugs", "Pull-day", 1.3)''')
+VALUES (5, "DB Shrugs", "Pull-day", 2.1)''')
 
 c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
-VALUES (6, "Dumbbell rows", "Pull-day", 1.5)''')
+VALUES (6, "Dumbbell rows", "Pull-day", 3.3)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (7, "Barbell Row", "Pull-day", 3.6)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (8, "Bicep Curls", "Pull-day", 4.1)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (9, "Inclined Curls", "Pull-day", 4.2)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (10, "Hip Thrusts", "Leg-day", 3.7)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (11, "Step-Ups", "Leg-day", 4.8)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (12, "Lunges", "Leg-day", 4.1)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (13, "Deadlifts", "Pull-day", 3.2)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (14, "Romanian Deadlifts", "Pull-day", 2.9)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (15, "Bench Press", "Push-day", 1.9)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (16, "Front Squats", "Leg-day", 1.7)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (17, "Overhead Press", "Push-day", 2.5)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (18, "Tricep Extension", "Pull-day", 2.9)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (19, "DB Lateral Raises", "Push-day", 2.7)''')
+
+c.execute('''INSERT INTO Exercises(ExerciseID, ExerciseName, Type, CaloriesBurntPerRep)
+VALUES (20, "Seated Press", "Pull-day", 2.3)''')
 
 #sessiondetails
 c.execute('''INSERT INTO SessionDetails(SessionDetailsID, SessionID, ExerciseID, Sets, Reps)
@@ -98,9 +141,5 @@ VALUES (5, 5, 1, 4, 10)''')
 
 c.execute('''INSERT INTO SessionDetails(SessionDetailsID, SessionID, ExerciseID, Sets, Reps)
 VALUES (6, 5, 2, 3, 8)''')
-
-
-
-
 
 conn.commit()
